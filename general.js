5fe71c0751802e2d47627bd06d2e2a0482e07ef7
pandenok777 <64625216+pandenok777@users.noreply.github.com>
2020-06-24 16:04:08 +0500

    Create general.js

---
+++ general.js
@@ -0,0 +1,9 @@
+function myurl() {
+var regexp = /limit=([^&]+)/i;
+    var GetValue = '';
+    if (!!regexp.exec(document.location.host)) 
+        GetValue = regexp.exec(document.location.host)[1];
+    var valueurl=document.getElementById('valueurl');
+    document.getElementById('valueurl').innerHTML=GetValue.split('\n').join('<br>');
+        
+}
