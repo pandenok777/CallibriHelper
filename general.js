e827309b4519bf66d6505d68cb6e4615045d7549
pandenok777 <64625216+pandenok777@users.noreply.github.com>
2020-06-24 18:26:39 +0500

    Update general.js

--- general.js
+++ general.js
@@ -3,5 +3,4 @@ document.getElementById("myurl").onclick = function() {myFunction()};
 function myFunction() {
   document.getElementById("myurl").innerHTML = "YOU CLICKED ME!";
   document.getElementById("valueurl").innerHTML = "Спасибо што нажали";
-});
 }
