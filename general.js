db0a376aa805ef73d436f3a531a8c74368d80218
pandenok777 <64625216+pandenok777@users.noreply.github.com>
2020-06-24 18:25:19 +0500

    Update general.js

--- general.js
+++ general.js
@@ -2,7 +2,6 @@ document.getElementById("myurl").onclick = function() {myFunction()};
 
 function myFunction() {
   document.getElementById("myurl").innerHTML = "YOU CLICKED ME!";
-  chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-    document.getElementById("valueurl").innerHTML = tabs;
+  document.getElementById("valueurl").innerHTML = "Спасибо што нажали";
 });
 }
