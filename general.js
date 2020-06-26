6c3735ad1f18a6ac76d78de5e8ebdd8cd427edae
pandenok777 <64625216+pandenok777@users.noreply.github.com>
2020-06-26 12:58:21 +0500

    Update general.js

--- general.js
+++ general.js
@@ -7,3 +7,10 @@ function myFunction() {
     document.getElementById("valueurl").innerHTML = url;
 });
 }
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  console.log('Looking on the page' + tab.url + ' element with desired classname');
+  chrome.tabs.executeScript({
+    code: 'var testElements = document.getElementsByClassName("callibri_phone"); var el = testElements[0]; if (el) {document.getElementById("callibriphonechecker").innerHTML = 'Опачки! Да на странице есть размеченные дефолтным классом элементы!';}'
+  });
+});
