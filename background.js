65f6dcf1b43e67381ce9ff6852cb629ab777be77
pandenok777 <64625216+pandenok777@users.noreply.github.com>
2020-06-30 15:44:02 +0500

    Update background.js

--- background.js
+++ background.js
@@ -1,1 +1,9 @@
+document.getElementById("myurl").onclick = function() {myFunction()};
 
+function myFunction() {
+  document.getElementById("myurl").innerHTML = "Я знаю на какой ты странице! Это:";
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    var url = tabs[0].url;
+    document.getElementById("valueurl").innerHTML = url;
+});
+}
