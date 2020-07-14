923c209c5d64c37cebc3a4e0f56c2c3befd68369
pandenok777 <64625216+pandenok777@users.noreply.github.com>
2020-07-14 11:18:45 +0500

    Update background.js
    добавил изменение строки чому

--- background.js
+++ background.js
@@ -5,7 +5,8 @@ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "callibri" && request.script != '' ) {
       chrome.browserAction.setIcon({ path: { "19": "icongreen-19.png",
-                                       "38": "icongreen-38.png" } });;
+                                       "38": "icongreen-38.png" } });
+      document.getElementById("checkscript").innerHTML = 'СРОБОТОЛО';
     }
     else
     {
