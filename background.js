40e264c8e2b49a9a216903bd517e48a98d7721a2
pandenok777 <64625216+pandenok777@users.noreply.github.com>
2020-11-24 23:16:08 +0500

    Delete background.js

--- background.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("myurl").onclick = function() {myFunction()};
-
-function myFunction() {
-  document.getElementById("myurl").innerHTML = "Я знаю на какой ты странице! Это:";
-  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-    var url = tabs[0].url;
-    document.getElementById("valueurl").innerHTML = url;
-});
-}
