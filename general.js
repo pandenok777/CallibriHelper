01047cac780d5c114830f96d0fc7f31436ae7fd1
pandenok777 <64625216+pandenok777@users.noreply.github.com>
2020-11-25 00:41:52 +0500

    Update general.js

--- general.js
+++ general.js
@@ -44,7 +44,7 @@ function myFunction() {
     nickname = nickname.replace(/[\9]/g,'𝟡');
     nickname = nickname.replace(/[\0]/g,'𝟘');
     document.getElementById("nicknamemodel").innerHTML = nickname + " + 🅿🅾🅲🅾🅼🅰🆇🅰 = :biglover ";
-    var copyText = nickname + " + 🅿🅾🅲🅾🅼🅰🆇🅰 = :biglover ";
+    var copyText = document.getElementById("nicknamemodel");
     copyText.select();
     document.execCommand("copy");
   });
