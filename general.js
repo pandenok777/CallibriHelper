425d4c37cc5b809802d611b04da8713a234c0aed
pandenok777 <64625216+pandenok777@users.noreply.github.com>
2020-12-18 15:55:45 +0500

    Update general.js

--- general.js
+++ general.js
@@ -1,7 +1,7 @@
 document.getElementById("myurl").onclick = function() {myFunction()};
 
 function myFunction() {
-  document.getElementById("myurl").innerHTML = "готово епта)) копируй хуле";
+  document.getElementById("myurl").innerHTML = "готово";
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     var url = tabs[0].url;
     //document.getElementById("valueurl").innerHTML = url;
